c6264d9d3f2943c0c0b6fc7241c98a83d50228f6
xisuo67 <392706283@qq.com>
2019-01-10 22:22:52 +0800

    缺陷修复
    代码提交

--- TTSHelperDemo/TTSHelperDemo/Program.cs
+++ TTSHelperDemo/TTSHelperDemo/Program.cs
@@ -23,10 +23,8 @@ namespace TTSHelperDemo
             string relativePath = string.Format("/UpLoad/Voice/{0}.mp3", $"{Guid.NewGuid().ToString("N")}{DateTime.Now.ToString("yyyyMMddHHmmss")}");
             XFPostModel model = new XFPostModel()
             {
-                //APIKey = "your APIKey",
-                //AppID = "your AppID",
-                APIKey = "5c1b8139",
-                AppID = "7cb6fffe91f30fe40fd766758ac52ad8",
+                APIKey = "your APIKey",
+                AppID = "your AppID",
                 AueType = AueType.Lame, //若保存此类型，文件后缀名请传mp3,若希望保存wav文件，AueType枚举类型为Raw，后缀名请传wav
                 FilePath = FileHelper.GetAbsolutePath(relativePath),
                 TextContext = "测试文本转语音"
